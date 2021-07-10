426b4be703a19b7f82df151156fbd331d1781f43
Fizioh <batrom@laposte.net>
2021-07-10 17:39:04 +0200

    extract text & insert in widget

--- app.py
+++ app.py
@@ -21,7 +21,17 @@ def open_file():
     browse_text.set("Chargement...")
     file = askopenfile(parent=root, mode='rb', title="Choisissez un fichier", filetype=[("Fichier PDF", "*.pdf")])
     if file:
-        print("Le fichier a été chargé avec succès !")
+        read_pdf = PyPDF2.PdfFileReader(file)
+        page = read_pdf.getPage(0)
+        page_content = page.extractText()
+        
+        text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
+        text_box.insert(1.0, page_content)
+        text_box.tag_configure("center", justify="center")
+        text_box.tag_add("center", 1.0, "end")
+        text_box.grid(column=1, row=3)
+        
+        browse_text.set("Selectionner un autre fichier")
 
 browse_text = tk.StringVar()
 browse_btn = tk.Button(root, textvariable=browse_text, command=lambda:open_file(), font="Raleway", bg="#20bebe", fg="white", height=3, width=20)
