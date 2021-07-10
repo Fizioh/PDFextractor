35594559eb0e34d77b9111c5f3aa3069b7e3f2b6
Fizioh <batrom@laposte.net>
2021-07-10 16:26:47 +0200

    resize tkinter canvas

---
+++ app.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import PyPDF2
+from PIL import Image, ImageTk
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width=600, height=300)
+canvas.grid(columnspan=3)
+
+
+root.mainloop()
