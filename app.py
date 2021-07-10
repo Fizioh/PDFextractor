9eff6fbd8dc2d1f52eca5307b4d0324be79ff502
Fizioh <batrom@laposte.net>
2021-07-10 16:53:33 +0200

    add logo widget

--- app.py
+++ app.py
@@ -7,5 +7,11 @@ root = tk.Tk()
 canvas = tk.Canvas(root, width=600, height=300)
 canvas.grid(columnspan=3)
 
+logo = Image.open('logo.png')
+logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(image=logo)
+logo_label.image = logo
+logo_label.grid(column=1, row=0)
+
 
 root.mainloop()
